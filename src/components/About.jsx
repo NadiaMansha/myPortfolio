7309d24d93794dace505af91ef3d77cfa220570e
Nadia <34932@gcslahore.du.pk>
2023-03-13 15:41:08 +0500

    fixed some errors

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 export default function About() {
-  return <div>
+  const profession="Full Stack Developer"
+  return <div
+  style={
+    {
+      marginTop: 150,
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center",
+      textAlign: "center",
+     backgroundColor: "white",
+
+
+  }
+  }
+  >
     <h1
     style={
       {
@@ -15,14 +30,21 @@ export default function About() {
     <p
     style={
       {
+        color:"black",
         fontSize: 20,
         fontWeight: "bold",
         marginBottom: 0,
       }
     }
-      
+
     >
-      I am a full stack web developer.I am a student Information Technology . I am passionate about building applications that are user friendly and easy to use. I am a self starter and a fast learner. I am always looking for new challenges and opportunities to learn and grow.
+     My name is Nadia Mansha.I am a student Information Technology . <br />
+     I am a  {profession}
+    
+     <br />
+      I am passionate about building applications that are user friendly and easy to use. <br />
+       I am a self starter and a fast learner. <br />
+      I am always looking for new challenges and opportunities to learn and grow.
     </p>
   </div>;
 }
